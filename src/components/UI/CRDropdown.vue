9773f86b1c9988b88591fbbdd9843d90aef7b178
Mani Mirjavadi <mani.mirjavadi@gmail.com>
2023-01-26 19:33:42 +0330

    [fix] DropDownMenu: Added emits

--- src/components/UI/CRDropdown.vue
+++ src/components/UI/CRDropdown.vue
@@ -2,32 +2,58 @@
 import { ref } from "vue";
 import SvgIcon from "@jamescoyle/vue-icon";
 import { mdiChevronDown as chevronDown } from "@mdi/js";
+import { useLoading } from "@/stores/loading";
 
+const emit = defineEmits<{
+  (e: "regionChanged", id: string): void;
+}>();
+const loadingHandler = useLoading();
+
+const regions = ref(["All", "Africa", "Americas", "Asia", "Europe", "Oceania"]);
 const dropDown = ref(false);
+const dropDownValue = ref("Filter by Region");
 
 const toggleDropDown = () => {
-  dropDown.value = !dropDown.value;
+  if (!loadingHandler.status) {
+    dropDown.value = !dropDown.value;
+  }
+};
+
+const changeRegion = (region: string) => {
+  dropDownValue.value = region;
+  dropDown.value = false;
+  emit("regionChanged", dropDownValue.value);
 };
 </script>
 
 <template>
+  <div v-if="dropDown" class="overlay" @click="() => (dropDown = false)"></div>
   <div class="my-8 w-[50%] md:max-w-[200px] relative inline-block">
-    <button class="cr_input" @click="toggleDropDown">
+    <button
+      class="cr_input"
+      @click="toggleDropDown"
+      :disabled="loadingHandler.status"
+    >
       <div class="iconContainer">
         <SvgIcon type="mdi" :path="chevronDown"></SvgIcon>
       </div>
-      Search by Region
+      {{ dropDownValue === "All" ? "Filter By Region" : dropDownValue }}
     </button>
     <transition name="dropdown">
-      <div v-show="dropDown">
+      <div class="z-50" v-show="dropDown">
         <div
           class="absolute mt-2 w-full bg-lightBg shadow-3xl dark:bg-darkElement rounded-md py-2 px-4"
           id="dropdown-menu"
         >
-          <a href="#" class="block px-4 py-1">Option 1</a>
-          <a href="#" class="block px-4 py-2">Option 2</a>
-          <a href="#" class="block px-4 py-2">Option 3</a>
-          <a href="#" class="block px-4 py-2">Option 4</a>
+          <a
+            v-for="region in regions"
+            :key="region"
+            href="#"
+            class="block px-4 py-1"
+            @click="changeRegion(region)"
+          >
+            {{ region }}
+          </a>
         </div>
       </div>
     </transition>
@@ -35,6 +61,10 @@ const toggleDropDown = () => {
 </template>
 
 <style scoped>
+.overlay {
+  @apply fixed w-full h-[200%];
+}
+
 button {
   @apply w-full h-[50px] flex justify-start items-center text-sm;
 }
