7e4f3dacefb1c7a2f7f85a72a349007d2df5dce4
Mani Mirjavadi <mani.mirjavadi@gmail.com>
2023-01-26 19:32:32 +0330

    [feat] loading: added loading

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,15 @@
 import { RouterView } from "vue-router";
 
 import TheHeader from "./components/common/TheHeader.vue";
+import CRLoading from "./components/UI/CRLoading.vue";
+import { useLoading } from "./stores/loading";
+
+const loadingHandler = useLoading();
 </script>
 
 <template>
+  <CRLoading v-if="loadingHandler.status" />
+
   <main>
     <TheHeader> Where in the world? </TheHeader>
     <RouterView />
