af90acd1ebb7c3243b6c83b766b05402c650d220
Mani Mirjavadi <mani.mirjavadi@gmail.com>
2023-01-26 17:38:27 +0330

    [fix] HomeView: Cleaned Up home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,47 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CRButton from "@/components/UI/CRButton.vue";
+import CRCard from "@/components/UI/CRCard.vue";
+import CRDropdown from "@/components/UI/CRDropdown.vue";
+import CRInput from "@/components/UI/CRInput.vue";
+import { useCountries } from "@/stores/countries";
+import { onMounted } from "vue";
+
+const countriesHandler = useCountries();
+
+onMounted(async () => {
+  await countriesHandler.fetchCountries();
+});
+</script>
 
 <template>
-  <main>Hello World</main>
+  <main class="mainContainer rp">
+    <div class="headerContainer">
+      <CRInput />
+      <CRDropdown />
+    </div>
+
+    <div class="countriesList">
+      <div
+        v-for="country in countriesHandler.countries"
+        :key="country.name?.common"
+      >
+        <CRCard
+          :image="country.flags?.svg"
+          :name="country.name?.common"
+          :population="country.population"
+          :region="country.region"
+          :capital="country.capital"
+        />
+      </div>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.headerContainer {
+  @apply md:flex md:justify-between md:items-center mt-4 md:mt-0;
+}
+.countriesList {
+  @apply w-full mx-auto mb-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4;
+}
+</style>
